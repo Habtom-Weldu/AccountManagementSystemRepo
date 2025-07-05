e5a35ecf604a027dd21264db1ca89afe45852dcd
Habtom Hailay <hailay.hab@gmail.com>
2025-07-05 15:04:46 -0700

    feat: Modify FileManager class to use config.properties class so that, safer database file path retrieval can be achieved

--- src/app/FileManager.java
+++ src/app/FileManager.java
@@ -1,8 +1,34 @@
 package app;
 
-import java.io.*;
-public class FileManager {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+//import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
 
+public class FileManager {
+    private static final Properties props = new Properties();
+
+    static {
+        try (InputStream input = new FileInputStream("config.properties")) {
+            props.load(input);
+        } catch (IOException e) {
+            System.err.println("⚠️ Failed to load config.properties: " + e.getMessage());
+        }
+    }
+
+    public static String getAccountsFilePath() {
+        String rawPath = props.getProperty("account.file.path", "data/accounts.txt");
+        return Paths.get(rawPath).toString();
+    }
+
+    public static String getDatabaseFilePath() {
+        String rawPath = props.getProperty("accountDatabase.file.path", "data/accountsDB.db");
+        return Paths.get(rawPath).toString();
+    }
+
+     /*
     // Folder and file name constants
     private static final String DATA_FOLDER = "data";
     private static final String FILE_NAME = "Accounts.txt";
@@ -24,5 +50,5 @@ public class FileManager {
                 System.out.println("⚠️ Failed to create data folder.");
             }
         }
-    }
+    } */
 }
