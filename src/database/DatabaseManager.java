1275d88d70c2c043a8c6b9182d8ca6a6eeeb9a6e
Habtom Hailay <hailay.hab@gmail.com>
2025-07-06 15:08:53 -0700

    feat: JDBC existance checking feature

--- src/database/DatabaseManager.java
+++ src/database/DatabaseManager.java
@@ -1,13 +1,38 @@
 package database;
 
-import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import app.util.FileManager;
 
 public class DatabaseManager {
+    private static final String DB_URL;
+
+    static {
+        String dbFilePath = FileManager.getDatabaseFilePath();
+        DB_URL = "jdbc:sqlite:" + dbFilePath;
+    }
+
+    public static Connection getDatabaseConnection() {
+        // check for SQLite JDBC driver exist
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            System.err.println("❌ SQLite JDBC driver not found. Please add sqlite-jdbc.jar to classpath.");
+            //throw new RuntimeException(e);
+            System.exit(1); // Exit the program with error status
+        }
+
+        try {
+            return DriverManager.getConnection(DB_URL); // creates Database if not existed
+        } catch (SQLException e) {
+            System.err.println("❌ Failed to connect to DB: " + e.getMessage());
+            return null;
+        }
+    }
+    /*
     // This class will handle connecting to SQLite database file.
-    //private static final String DB_URL = "jdbc:sqlite:accountsDB.db"; // stored in project root
+    // private static final String DB_URL = "jdbc:sqlite:accountsDB.db"; // stored in project root
     private static final String DB_FILE_PATH = "data/accountsDB.db";
 
     public static Connection getConnection() throws SQLException {
@@ -26,5 +51,5 @@ public class DatabaseManager {
             System.err.println("❌ Failed to connect to DB: " + e.getMessage());
             return null;
         }
-    }
+    } */
 }
