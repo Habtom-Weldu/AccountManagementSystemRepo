896de2338154ded2b7139ef423d2ba8f01af1847
Habtom Hailay <hailay.hab@gmail.com>
2025-08-07 01:13:55 -0700

    Add unit test for readIntInRange in InputValidator

--- test/app/util/InputValidatorTest.java
+++ test/app/util/InputValidatorTest.java
@@ -1,2 +1,12 @@
-package app;public class InputValidatorTest {
+package app;
+public class InputValidatorTest {
+    @Test
+    public void testReadIntInRange_validInput() {
+        String simulatedInput = "abc\n4\n"; // First input invalid, second valid
+        Scanner testScanner = new Scanner(simulatedInput);
+        InputValidator.setScanner(testScanner);
+
+        int result = InputValidator.readIntInRange(1, 5, "Enter a number: ");
+        assertEquals(4, result);
+    }
 }
