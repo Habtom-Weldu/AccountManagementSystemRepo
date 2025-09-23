5558b2cffb3d5bf460dbb804715e5ba5e521863c
Habtom <hailay.hab@gmail.com>
2025-09-22 23:22:48 -0700

    modification: a simple keywords correction in the InputValidatorPhoneNumberTest class

--- test/app/util/InputValidatorPhoneNumberTest.java
+++ test/app/util/InputValidatorPhoneNumberTest.java
@@ -1,11 +1,13 @@
 package app.util;
 
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InputValidatorPhoneNumberTest {
+    @ParameterizedTest
     @MethodSource("providePhoneNumbers")
     void testIsValidPhoneNumber(String input, boolean expectedValid) {
         ValidationResult result = InputValidator.isValidPhoneNumber(input);
